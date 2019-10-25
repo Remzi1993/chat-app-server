bcf865ae17de44453fcc17ca1942d4dc5d9dcd5e
Remzi <ict@remzi.info>
2019-10-25 11:49:17 +0200

    Setup stream

--- stream/router.js
+++ stream/router.js
@@ -1,12 +1,21 @@
 const {Router} = require('express')
 const Chatroom = require('./model')
+const Sse = require('json-sse')
 
 const router = new Router()
+const stream = new Sse()
 
 
-router.get('/stream', (req, res) => {
+router.get('/stream', async (req, res) => {
     console.log('got a request on /stream')
-    res.send('it works!')
+
+    const messages = await Chatroom.findAll()
+    const data = JSON.stringify(messages)
+    console.log('Stringifly', data)
+    
+    stream.updateInit(data)
+    stream.init(req, res)
+
 })
 
 router.post('/message', async (req, res) => {
@@ -15,6 +24,14 @@ router.post('/message', async (req, res) => {
     const entity = await Chatroom.create({
         message
     })
+
+    const messages = await Chatroom.findAll()
+    const data = JSON.stringify(messages)
+    // console.log('Stringifly', data)
+    stream.send(data)
+
+
+
     res.status(201)
     res.send('Thanks for your message')
 })
